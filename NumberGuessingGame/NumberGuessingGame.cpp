cc9bf1aa80a65a34c6ad3fe0e3155a9f989a8d68
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-07-19 12:58:04 -0400

    set range

--- NumberGuessingGame/NumberGuessingGame.cpp
+++ NumberGuessingGame/NumberGuessingGame.cpp
@@ -1,8 +1,16 @@
 #include<iostream>
 
-
-int main()
+void get_range()
 {
 	int range_start, range_end;
-	
+	std::cout << "Range: What do you want the smallest number to be? ";
+	std::cin >> range_start;
+	std::cout << "Range: What do you want the largest number to be? ";
+	std::cin >> range_end;
+
+	std::cout << "Range set to " << range_start << " & " << range_end << std::endl;
+}
+int main()
+{
+	get_range();
 }
