2d3712f7a7ed20fa67f66d051c8a387651049246
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-07-19 13:10:24 -0400

    finished game

--- NumberGuessingGame/NumberGuessingGame.cpp
+++ NumberGuessingGame/NumberGuessingGame.cpp
@@ -1,8 +1,10 @@
 #include<iostream>
 
+int range_start, range_end;
+int answer_req;
+int res;
 void get_range()
 {
-	int range_start, range_end;
 	std::cout << "Range: What do you want the smallest number to be? ";
 	std::cin >> range_start;
 	std::cout << "Range: What do you want the largest number to be? ";
@@ -10,7 +12,43 @@ void get_range()
 
 	std::cout << "Range set to " << range_start << " & " << range_end << std::endl;
 }
+
+void select_randon_in_range()
+{
+	res = rand() % range_end + range_start;
+}
+void check_answer()
+{
+	if (answer_req == res)
+	{
+		std::cout << "Correct! The number was " << res << "!" << std::endl;
+	}
+	else
+	{
+		std::cout << "Incorrect. The answer was " << res << std::endl;
+	}
+}
+void answer()
+{
+	std::cout << "Type your answer: ";
+	std::cin >> answer_req;
+
+		check_answer();
+
+}
+
 int main()
 {
 	get_range();
+
+	if (!range_start || !range_end)
+	{
+		std::cout << "You must specify to ranges" << std::endl;
+		get_range();
+	}
+	else
+	{
+		select_randon_in_range();
+		answer();
+	}
 }
